3043d3829015a60cd70974cad7944cd923a23b60
Edoardo <edoardo.morena@mail.polimi.it>
2023-07-11 16:15:58 +0200

    model importing and input preparation attempt

--- MagicKnob/src/PluginEditor.h
+++ MagicKnob/src/PluginEditor.h
@@ -32,9 +32,7 @@ class MagicKnobEditor  :   public juce::AudioProcessorEditor,
                           // listen to buttons
                           public juce::Button::Listener, 
                           // listen to sliders
-                          public juce::Slider::Listener, 
-                          // listen to piano keyboard widget
-                          private juce::MidiKeyboardState::Listener
+                          public juce::Slider::Listener 
                           
 
 {
@@ -48,17 +46,10 @@ public:
 
     void sliderValueChanged (juce::Slider *slider) override;
     void buttonClicked(juce::Button* btn) override;
-    // from MidiKeyboardState
-    void handleNoteOn(juce::MidiKeyboardState *source, int midiChannel, int midiNoteNumber, float
- velocity) override; 
-     // from MidiKeyboardState
-    void handleNoteOff(juce::MidiKeyboardState *source, int midiChannel, int midiNoteNumber, float velocity) override; 
+
 
     //juce::AudioBuffer<float> getAudioBufferFromFile(juce::File file); //POTREBBE TORNARE UTILE
 
-
-    //MODEL INFERENCE
-    void InferModels(std::vector<torch::jit::IValue> my_input, torch::Tensor phase, int size);
 
     
 
@@ -67,15 +58,10 @@ private:
 
     juce::ToggleButton envToggle; 
     juce::Slider superKnob;
-    juce::Slider modIndexSlider;
-    juce::Slider modDepthSlider;
 
     // torch::nn::Linear linear{1, 2}; 
     //NeuralNetwork nn{1, 2};
 
-    // needed for the mini piano keyboard
-    juce::MidiKeyboardState kbdState;
-    juce::MidiKeyboardComponent miniPianoKbd; 
     
     //------------------------------------------------------------------------------------
     MagicKnobProcessor& audioProcessor;
